05bde460eaf8e0baae4851516c41372a0a1bb158
姚旭 <xu.yao@zeropartner.com>
2023-03-06 14:13:03 +0800

    update slicekit

--- slicekit/slice.go
+++ slicekit/slice.go
@@ -1,5 +1,7 @@
 package slicekit
 
+import "math"
+
 // Includes Includes[T comparable]
 //
 //	@Description: 切片中是否包含指定元素
@@ -53,6 +55,171 @@ func IncludesSubSlice[T comparable](s []T, sub []T) bool {
 	return true
 }
 
+// Each Each[T any]
+//
+//	@Description: 循环遍历
+//	@param s
+//	@param iteratee
+func Each[T any](s []T, iteratee func(idx int, item T)) {
+	for index, val := range s {
+		iteratee(index, val)
+	}
+}
+
+// Chunk Chunk[T any]
+//
+//	@Description: 按照指定size 切分数组
+//	@param s
+//	@param size
+//	@return [][]T
+func Chunk[T any](s []T, size int) [][]T {
+	if size >= len(s) {
+		r := make([][]T, 1, 1)
+		r = append(r, s)
+		return r
+	}
+	capSize := int(math.Ceil(float64(len(s) / size)))
+	result := make([][]T, 0, capSize)
+
+	for i := 0; i < capSize; i++ {
+		startIdx := i * size
+		endIndex := startIdx + size
+		if endIndex >= len(s) {
+			endIndex = len(s)
+		}
+
+		result = append(result, s[startIdx:endIndex])
+	}
+
+	return result
+}
+
+// Compact Compact[T comparable]
+//
+//	@Description:创建一个新数组，包含原数组中所有的非假值元素
+//	@param s
+//	@return []T
+func Compact[T comparable](s []T) []T {
+	var zero T
+
+	result := make([]T, 0, len(s))
+	for _, val := range s {
+		if val != zero {
+			result = append(result, val)
+		}
+	}
+
+	return result
+}
+
+// Concat Concat[T any]
+//
+//	@Description: 创建一个新切片，将多个切片连接在一起
+//	@param s
+//	@param slices
+//	@return []T
+func Concat[T any](s []T, slices ...[]T) []T {
+	result := append([]T{}, s...)
+
+	for _, val := range slices {
+		result = append(result, val...)
+	}
+	return result
+}
+
+// Difference Difference[T comparable]
+//
+//	@Description: 过滤掉指定元素 并返回一个新的切片
+//	@param s
+//	@param ignoreSlices
+//	@return []T
+func Difference[T comparable](s []T, ignoreSlices []T) []T {
+
+	result := make([]T, 0, len(s))
+
+	for _, val := range s {
+		if !Includes(ignoreSlices, val) {
+			result = append(result, val)
+		}
+	}
+
+	return result
+}
+
+// DifferenceWith DifferenceWith[T comparable]
+//
+//	@Description: 自定义规则过滤掉指定元素 并返回一个新的切片
+//	@param s
+//	@param ignoreSlices
+//	@param comparator
+//	@return []T
+func DifferenceWith[T comparable](s []T, ignoreSlices []T, comparator func(item1, item2 T) bool) []T {
+	result := make([]T, 0, cap(s))
+
+	checkHit := func(arr []T, it T, comparator func(item1, item2 T) bool) bool {
+		isHit := false
+
+		for _, v := range arr {
+			if comparator(it, v) {
+				isHit = true
+				break
+			}
+		}
+
+		return isHit
+	}
+
+	for _, val := range s {
+		if !checkHit(ignoreSlices, val, comparator) {
+			result = append(result, val)
+		}
+	}
+
+	return result
+
+}
+
+// Equal Equal[T comparable]
+//
+//	@Description: 判断两个切片是否相等
+//	@param s1
+//	@param s2
+//	@return bool
+func Equal[T comparable](s1, s2 []T) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	for index, _ := range s1 {
+		if s1[index] != s2[index] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// EqualWith EqualWith[T comparable]
+//
+//	@Description: 按照指定规则 判断两个切片是否相等
+//	@param s1
+//	@param s2
+//	@param comparator
+//	@return bool
+func EqualWith[T comparable](s1, s2 []T, comparator func(v1, v2 T) bool) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	for i := 0; i < len(s1); i++ {
+		if !comparator(s1[i], s2[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
 // Filter Filter[T interface{}]
 //
 //	@Description: 筛选符合条件的切片
@@ -60,7 +227,7 @@ func IncludesSubSlice[T comparable](s []T, sub []T) bool {
 //	@param predicate
 //	@return []T
 func Filter[T interface{}](s []T, predicate func(idx int, item T) bool) []T {
-	var sliceList []T
+	sliceList := make([]T, 0, cap(s))
 
 	for index, val := range s {
 		if predicate(index, val) {
@@ -71,6 +238,24 @@ func Filter[T interface{}](s []T, predicate func(idx int, item T) bool) []T {
 	return sliceList
 }
 
+// FilterMap FilterMap[T any, D any]
+//
+//	@Description: 按照 规则 筛选并生成新的切片
+//	@param s
+//	@param iteratee
+//	@return []D
+func FilterMap[T any, D any](s []T, iteratee func(idx int, item T) (D, bool)) []D {
+	result := make([]D, 0, cap(s))
+
+	for index, val := range s {
+		if res, ok := iteratee(index, val); ok {
+			result = append(result, res)
+		}
+	}
+
+	return result
+}
+
 // Map Map[T interface{}]
 //
 //	@Description: 按照规则 生成一个新数组
@@ -78,12 +263,11 @@ func Filter[T interface{}](s []T, predicate func(idx int, item T) bool) []T {
 //	@param iteratee
 //	@return []T
 func Map[T interface{}](s []T, iteratee func(idx int, item T) T) []T {
-	var sliceList []T
+	result := make([]T, len(s), cap(s))
 
 	for index, val := range s {
-		result := iteratee(index, val)
+		result[index] = iteratee(index, val)
 
-		sliceList = append(sliceList, result)
 	}
-	return sliceList
+	return result
 }
--- slicekit/slice_test.go
+++ slicekit/slice_test.go
@@ -2,6 +2,7 @@ package slicekit
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -98,4 +99,86 @@ func TestMap(t *testing.T) {
 
 	fmt.Println(list1)
 
+}
+
+func TestChunk(t *testing.T) {
+	arr := []string{"a", "b", "c", "d", "e"}
+
+	result1 := Chunk(arr, 1)
+	result2 := Chunk(arr, 2)
+	result3 := Chunk(arr, 3)
+	result4 := Chunk(arr, 4)
+	result5 := Chunk(arr, 5)
+	result6 := Chunk(arr, 6)
+
+	fmt.Println(result1)
+	fmt.Println(result2)
+	fmt.Println(result3)
+	fmt.Println(result4)
+	fmt.Println(result5)
+	fmt.Println(result6)
+}
+
+func TestCompact(t *testing.T) {
+
+	var s1 = []int{0, 1, 2}
+	var s2 = []string{"0", "1", ""}
+	var s3 = []bool{false, true, true}
+
+	r1 := Compact(s1)
+	r2 := Compact(s2)
+	r3 := Compact(s3)
+
+	fmt.Println(r1)
+	fmt.Println(r2)
+	fmt.Println(r3)
+
+	var list = [3]int{1, 2, 3}
+
+	typeOfList := reflect.TypeOf(list)
+	fmt.Println(typeOfList.Kind())
+
+	typeOfS1 := reflect.TypeOf(s1)
+	fmt.Println(typeOfS1.Kind())
+}
+
+func TestDifferenceWith(t *testing.T) {
+	slice1 := []int{1, 2, 3, 4, 5}
+	slice2 := []int{4, 5, 6, 7, 8}
+
+	isDouble := func(v1, v2 int) bool {
+		return v2 == 2*v1
+	}
+
+	result := DifferenceWith(slice1, slice2, isDouble)
+
+	fmt.Println(result)
+}
+
+func TestEqual(t *testing.T) {
+	slice1 := []int{1, 2, 3}
+	slice2 := []int{1, 2, 3}
+	slice3 := []int{1, 3, 2}
+
+	result1 := Equal(slice1, slice2)
+	result2 := Equal(slice1, slice3)
+
+	fmt.Println(result1)
+	fmt.Println(result2)
+}
+
+func TestEqualWith(t *testing.T) {
+	slice1 := []int{1, 2, 3}
+	slice2 := []int{2, 4, 6}
+
+	isDouble := func(a, b int) bool {
+		return b == a*2
+	}
+
+	result1 := EqualWith(slice1, slice2, isDouble)
+
+	fmt.Println(result1)
+
+	// Output:
+	// true
 }
